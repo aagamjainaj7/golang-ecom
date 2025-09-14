5a053364912c64aefa4bf076abff22887cecf740
aagamjainaj7 <aagamjainaj7@gmail.com>
2025-09-14 11:42:22 +0000

    added routes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,8 +5,11 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/aagamjainaj7/golang-ecom/services/user"
 	"github.com/gorilla/mux"
+	"github.com/aagamjainaj7/golang-ecom/services/cart"
+	"github.com/aagamjainaj7/golang-ecom/services/order"
+	"github.com/aagamjainaj7/golang-ecom/services/product"
+	"github.com/aagamjainaj7/golang-ecom/services/user"
 )
 
 type APIServer struct {
@@ -24,10 +27,24 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
 
-	log.Println("Listening on ", s.addr)
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore, userStore)
+	productHandler.RegisterRoutes(subrouter)
+
+	orderStore := order.NewStore(s.db)
+
+	cartHandler := cart.NewHandler(productStore, orderStore, userStore)
+	cartHandler.RegisterRoutes(subrouter)
+
+	// Serve static files
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
+
+	log.Println("Listening on", s.addr)
+
 	return http.ListenAndServe(s.addr, router)
 }
